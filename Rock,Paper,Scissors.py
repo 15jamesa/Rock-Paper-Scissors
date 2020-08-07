c30de275a9e1a0087aef9ed7b0c65c8eb5e9471a
15jamesa <avaraver.aj@gmail.com>
2020-08-07 17:00:29 +0100

    Neater Code

--- Rock,Paper,Scissors.py
+++ Rock,Paper,Scissors.py
@@ -1,21 +1,24 @@
 from appJar import gui
 import random
 
+
 def decision(btn):
     selection = ["Rock", "Paper", "Scissors"]
     comp = random.choice(selection)
     result = "You chose: " + btn + "\n\nThe computer chose: " + comp
     rps.setLabel("Instructions", result)
-    if (btn=="Rock" and comp=="Scissors") or (btn=="Paper" and comp=="Rock") or (btn=="Scissors" and comp=="Paper"):
+    if (btn == "Rock" and comp == "Scissors") or (btn == "Paper" and comp == "Rock") or (btn == "Scissors" and comp == "Paper"):
         rps.setLabel("Results", "Player wins!")
-    elif (comp=="Rock" and btn=="Scissors") or (comp=="Paper" and btn=="Rock") or (comp=="Scissors" and btn=="Paper"):
+    elif (comp == "Rock" and btn == "Scissors") or (comp == "Paper" and btn == "Rock") or (comp == "Scissors" and btn == "Paper"):
         rps.setLabel("Results", "Computer wins!")
     else:
         rps.setLabel("Results", "It's a tie!")
 
+
 def refresh():
     rps.setLabel("Instructions", "Choose either Rock, Paper, or Scissors:")
     rps.setLabel("Results", "")
+
 
 def blank():
     pass
@@ -26,7 +29,7 @@ rps = gui("Rock, Paper, Scissors", "600x250")
 rps.addLabel("Results", "", 1, 0, 4)
 rps.addLabel("Instructions", "Choose either Rock, Paper, or Scissors:", 0, 0, 4)
 rps.setSticky("nesw")
-rps.setBg("Azure", override=False)
+rps.setBg("Azure")
 
 rps.addButton("Rock", decision, 2, 0)
 rps.addButton("Paper", decision, 2, 1)
